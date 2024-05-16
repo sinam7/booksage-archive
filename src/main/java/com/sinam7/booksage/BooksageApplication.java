82535ea9ddbd631d591eee27f4ffea1dc7da13df
sinam7 <muwon777@naver.com>
2024-05-16 12:20:21 +0900

    initial commit

---
+++ src/main/java/com/sinam7/booksage/BooksageApplication.java
@@ -0,0 +1,13 @@
+package com.sinam7.booksage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksageApplication.class, args);
+    }
+
+}
